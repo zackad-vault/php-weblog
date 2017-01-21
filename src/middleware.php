54a9b73dd873fd6df57500dd1357ef1d0f30baab
zackad <zackad.dev@gmail.com>
2017-01-21 09:32:38 +0700

    redirect all route to trailing slash counterpart

---
+++ src/middleware.php
@@ -0,0 +1,26 @@
+<?php
+// Application middleware
+
+// e.g: $app->add(new \Slim\Csrf\Guard);
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+$app->add(function (Request $request, Response $response, callable $next) {
+    $uri = $request->getUri();
+    $path = $uri->getPath();
+    if ($path != '/' && substr($path, -1) !== '/') {
+        // permanently redirect paths without a trailing slash
+        // to their trailing counterpart
+        $uri = $uri->withPath($path . '/');
+        // $uri = $uri->withPath(substr($path, 0, -1));
+        
+        if ($request->getMethod() == 'GET') {
+            return $response->withRedirect((string)$uri, 301);
+        } else {
+            return $next($request->withUri($uri), $response);
+        }
+    }
+
+    return $next($request, $response);
+});
