e49c0b876c6f68db15fd5dd6fe41d883d6569591
zackad <zackad.dev@gmail.com>
2017-01-22 13:55:28 +0700

    register twig-view container

--- src/dependencies.php
+++ src/dependencies.php
@@ -20,6 +20,27 @@ $container['view'] = function ($c) {
     return $view;
 };
 
+// Register Twig View helper
+$container['twig'] = function ($c) {
+    $settings = $c->get('settings')['twig'];
+    $view = new \Slim\Views\Twig($settings['template_path'], [
+        // 'cache' => 'path/to/cache'
+        'debug' => $settings['debug'],
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+    $view->addExtension(new Twig_Extension_Debug);
+    $view->addExtension(new Jralph\Twig\Markdown\Extension(
+        new Jralph\Twig\Markdown\Parsedown\ParsedownExtraMarkdown
+    ));
+    $view['debug'] = $settings['debug'];
+    $view['session'] = $_SESSION;
+
+    return $view;
+};
+
 // monolog
 $container['logger'] = function ($c) {
     $settings = $c->get('settings')['logger'];
