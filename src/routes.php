45c101e1112f0bd68cf9ee17ef37927e7b2abe66
zackad <zackad.dev@gmail.com>
2017-01-23 17:34:47 +0700

    redirect if post doesn't have or the slug is wrong

--- src/routes.php
+++ src/routes.php
@@ -6,8 +6,8 @@ $app->get('/', function ($request, $response, $args) {
     $this->logger->info("Slim-Skeleton '/' route");
 
     // Render index view
-    return $this->view->render('index.twig', $args);
-});
+    return $this->view->render($response, 'index.twig', $args);
+})->setName('homepage');
 
 $app->get('/{type:latest|popular|random}/', function ($request, $response, $args) {
     $this->logger->info("'/post/".$args['type']."/' route");
@@ -17,23 +17,40 @@ $app->get('/{type:latest|popular|random}/', function ($request, $response, $args
     foreach ($data['postlist'] as $key => $value) {
         $data['postlist'][$key]['slug'] = $article->slugify($value['title']);
     }
-    return $this->view->render('post-list.twig', $data);
+    return $this->view->render($response, 'post-list.twig', $data);
 });
 
-// $app->get('/post/[{id:[0-9]+}/]', function ($request, $response, $args) {
 $app->get('/post/[{id:[0-9]+}/[{slug}/]]', function ($request, $response, $args) {
     $this->logger->info("'/post' route");
 
+    // Initiate a
     $article = new Models\Article;
     $post = $article->getItemById($args['id']);
+    if (empty($post)) {
+        return $this->view->render($response, 'errors/404-notfound.twig');
+    }
+    $slug = $article->slugify($post['title']);
+
+    // Redirect article post if slug is wrong or not exists
+    if (!isset($args['slug']) || $args['slug'] !== $slug) {
+        $uri = $request->getUri();
+        $path = $uri->getPath();
+        $redirectPath = $this->router->pathFor('article', ['id' => $args['id']]) . $slug .'/';
+        return $response->withRedirect((string)$redirectPath, 302);
+    }
+
+    // Update article view counter
     $article->updateViewCount($post['id']);
+
+    // Data to be passed to view renderer
     $data = [
         'title' => 'Test Post',
         'post' => $post,
         'id' => $args['id'],
+        'args' => $args,
     ];
-    return $this->view->render('post.twig', $data);
-});
+    return $this->view->render($response, 'post.twig', $data);
+})->setName('article');
 
 $app->get('/tags[/]', function ($request, $response, $args) {
     $this->logger->info("'/tags' route");
