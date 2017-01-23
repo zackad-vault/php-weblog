e9f410e63de1e7debabf4a691740dca05193fcd7
zackad <zackad.dev@gmail.com>
2017-01-23 17:33:54 +0700

    authentication injector, switch to slim/twig-view renderer

--- src/dependencies.php
+++ src/dependencies.php
@@ -3,25 +3,31 @@
 
 $container = $app->getContainer();
 
-// view twig renderer
-$container['view'] = function ($c) {
-    $settings = $c->get('settings')['twig'];
-    $loader = new Twig_Loader_Filesystem($settings['template_path']);
-    $view = new Twig_Environment($loader, [
-        'debug' => $settings['debug'],
-    ]);
-    $view->addExtension(new Twig_Extension_Debug);
-    $view->addExtension(new Jralph\Twig\Markdown\Extension(
-        new Jralph\Twig\Markdown\Parsedown\ParsedownExtraMarkdown
-    ));
-    $view->addGlobal("debug", $settings['debug']);
-    $view->addGlobal("session", $_SESSION);
-
-    return $view;
+// Authentication
+$container['auth'] = function ($c) {
+    $auth = new Models\Auth;
+    return $auth;
 };
 
+// view twig renderer
+// $container['view'] = function ($c) {
+//     $settings = $c->get('settings')['twig'];
+//     $loader = new Twig_Loader_Filesystem($settings['template_path']);
+//     $view = new Twig_Environment($loader, [
+//         'debug' => $settings['debug'],
+//     ]);
+//     $view->addExtension(new Twig_Extension_Debug);
+//     $view->addExtension(new Jralph\Twig\Markdown\Extension(
+//         new Jralph\Twig\Markdown\Parsedown\ParsedownExtraMarkdown
+//     ));
+//     $view->addGlobal("debug", $settings['debug']);
+//     $view->addGlobal("session", $_SESSION);
+
+//     return $view;
+// };
+
 // Register Twig View helper
-$container['twig'] = function ($c) {
+$container['view'] = function ($c) {
     $settings = $c->get('settings')['twig'];
     $view = new \Slim\Views\Twig($settings['template_path'], [
         // 'cache' => 'path/to/cache'
